c745244b01759249abf8cc6b4323ed72eb1d3479
Richie Bendall <richiebendall@gmail.com>
2020-07-22 16:35:19 +1200

    Refactor

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 "use strict"
 
-module.exports = (...values) => values.slice(1).every((val, i) => val > values[i])
+module.exports = (...values) => values.slice(1).every((value, index) => value > values[index])
